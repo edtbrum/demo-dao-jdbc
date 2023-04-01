10cc025f8e3ddcb446dd1da0b0b357871a1349ee
edtbrum <edtbrum@gmail.com>
2023-04-01 08:49:37 -0300

    Classe Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,18 @@
 package application;
 
+import java.util.Date;
+
+import model.entities.Department;
+import model.entities.Seller;
+
 public class Program {
 
 	public static void main(String[] args) {
 
+		Department dp = new Department(2, "Books");
+		Seller se = new Seller(1,"Eduardo","edu@gmail.com",new Date(),3000.0,dp);
+		
+		System.out.println(se);
 	}
 
 }
